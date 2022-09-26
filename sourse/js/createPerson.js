b8f9bced0d964414d02eeeb683e8e9c5d1acfad7
Pavel.I <ivannikov.pavel1@mail.ru>
2022-09-27 00:08:56 +0300

    continue creating user

---
+++ sourse/js/createPerson.js
@@ -0,0 +1,4 @@
+function createPerson(){
+    document.querySelector('.createPerson').innerHTML += `<tr><td>1</td><td>Иванников Павел Андреевич</td><td>01.08.2001</td><td>Муж</td><td>+79622639938</td>
+    <td>ivannikov.pavel1@mail.ru</td></tr>`
+}
